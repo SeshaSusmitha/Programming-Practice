7831b8b17383d7701f8f72b187559b3cecf34c5b
SeshaSusmitha <yellamraju.susmita@gmail.com>
2017-10-23 10:57:11 -0700

    Working Code

--- best-time-to-sell-stocks.cpp
+++ best-time-to-sell-stocks.cpp
@@ -1,4 +1,4 @@
-/* Program URL : */
+/* Program URL : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/ */
 
 
 #include <iostream>
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main(){
-	vector<int> nums, res_vec;
+	vector<int> nums;
 	
 	nums.push_back(7);
 	nums.push_back(1);
@@ -15,16 +15,15 @@ int main(){
 	nums.push_back(3);
 	nums.push_back(6);
 	nums.push_back(4);
-	int len = nums.size(), val, prev_val, max = -99999;
-	for(int i = 0; i < len; i++){
-		for(int j = i + 1; j < len; j++){
-			val = nums[j] - nums[i];
-			cout << " val  " << val << " ";
-			max = (val > max) ? val : max;
-			//prev_val = val;
-		}
+	int len = nums.size(), ret = 0, max = 0;
+	for(int i = 1; i < len; i++){
+		ret = ret + nums[i] - nums[i-1];
+		if(ret < 0)
+			ret = 0;
+		if(ret > max)
+			max = ret;
 	}
 
-	cout << endl << "max = " << max << endl;
+	cout << "max = " << max << endl;
 
 }
